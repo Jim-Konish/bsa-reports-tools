7818450fa4b0040394bc3aadf1e7c217ef9c0ec3
Jim Konish <jim.konish@sphero.com>
2024-09-15 12:21:17 -0400

    Add a yml reader for email login info

--- login/get_login.py
+++ login/get_login.py
@@ -5,7 +5,7 @@ import pathlib
 def get_email_login():
     with open(pathlib.Path('login.yml'), "r") as f:
         login_data = yaml.safe_load(f)
-        return (login_data['email'], login_data['app_password'])
+        return (login_data['email'], login_data['app_password'], login_data['test_recipient'])
 
 def main():
     print(get_email_login())
